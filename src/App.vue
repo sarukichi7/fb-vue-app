21dfaee2e06c7940addf86b75c05754f4310d805
sarukichi7 <q.saruki@gmail.com>
2018-08-19 01:41:27 +0900

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+<div id="App">
+  <Home v-if="!isLogin"></Home>
+  <Editor v-if="isLogin"></Editor>
+</div>
+</template>
+
+<script>
+import Home from './components/Home.vue';
+import Editor from './components/Editor.vue';
+
+export default {
+  name: 'app',
+  data() {
+  	return {
+  	  isLogin: false
+  	}
+  },
+  created: function() {
+  	firebase.auth().onAuthStateChanged(user => {
+  	  console.log(user);
+  	  if (user) {
+  	  	this.isLogin = true;
+  	  } else {
+  	  	this.isLogin = false;
+  	  };
+  	});
+  },
+  components: {
+  	'Home': Home,
+  	'Editor': Editor,
+  },
+}
+</script>
